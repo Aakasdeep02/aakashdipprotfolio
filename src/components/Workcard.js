250f1934799a9291ed635b32d433de10220d0baa
Aakasdeep02 <109851089+Aakasdeep02@users.noreply.github.com>
2023-01-28 15:44:06 +0530

    changes in css

--- src/components/Workcard.js
+++ src/components/Workcard.js
@@ -13,7 +13,6 @@ const Workcard = (props) => {
                 <p>{props.text}</p>
                 <div className="pro-btn">
                     <NavLink to={props.view} className="btn">View</NavLink>
-                    <NavLink to={props.view} className="btn">Source</NavLink>
                 </div>
             </div>
         </div>
