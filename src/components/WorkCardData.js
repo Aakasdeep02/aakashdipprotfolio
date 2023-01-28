b98f2a6789815f87c437092337144cd902028d7c
Aakasdeep02 <109851089+Aakasdeep02@users.noreply.github.com>
2023-01-28 17:53:29 +0530

    ADD PROJECTS

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -5,14 +5,14 @@ const ProjectCardData = [
     {
         imgsrc: pro1,
         title: "Dictinary And Definition",
-        text: "This app will provide definition of word you will search. I have use dictionary api from radipapi. To built frontend I have use HTML and BootStrap. I have use javascript to merge api.",
-        view: "url.com",
+        text: "This app will provide definition of word you will search. I have use dictionary api from radipapi. To built frontend I have use HTML and BootStrap. I have use javascript to merge api.(Please click view button to get source code of project)",
+        view: "https://drive.google.com/drive/folders/15bkmrbvu9ErRWvusA9s3QM7kCpDpmpvP?usp=share_link",
     },
     {
         imgsrc: pro1,
-        title: "E-Commerce Website",
-        text: "E-commerce (electronic commerce) is the buying and selling of goods and services,or the transmitting of funds or data, over an electronic network, primarily the internet. These business transactions occur either as business-to-business (B2B) business-to-consumer (B2C), consumer-to-consumer or consumer-to-business.",
-        view: "url.com",
+        title: "Ajio Clone",
+        text: "This a clone of E-commerce website AJIO. I have use HTML and CSS.(I HAVE JUST MADE THE FROTEND OF WEBSITE, IT IS NOT INTERACTIVE AND OPEN IN DESKTOP. CLICK VIEW BUTTON FOR WEBSITE)",
+        view: "https://ajio-clone-aakasdeep.netlify.app/",
     },
     {
         imgsrc: pro1,
