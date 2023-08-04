befd85d52c65ce5a623d72e858b460a0ca7cbc81
Aakasdeep02 <109851089+Aakasdeep02@users.noreply.github.com>
2023-08-04 11:05:23 +0530

    Add Resume

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -2,6 +2,7 @@ import "./HeroimgStyle.css";
 import React from 'react'
 import { Link } from "react-router-dom";
 import Introimg from "../assets/heroimg.png";
+import Resumefile from "../assets/Aakasdeep.pdf";
 
 const Heroimg = () => {
   return (
@@ -14,6 +15,7 @@ const Heroimg = () => {
           <p>HI, I'M A FREELANCER.</p>
           <h1>Programmer And Developer.</h1>
           <Link to="/project" className="btn">Project</Link>
+          <Link to={Resumefile} download="Aakasdeep-PDF-document" target="-blank" rel="noreferrer" className="btn">Resume</Link>
         </div>
     </div>
   )
